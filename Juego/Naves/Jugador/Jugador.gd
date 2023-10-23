412898ddbb1260d08ba4ee3a4b1ecd18122f4a41
Lauta54 <landriel9169@gmail.com>
2023-10-23 16:56:36 -0300

    Propiedades player
    Disparo principal:
    -Proyectil
    -Cañón
    -Disparo
    -Eventos
    Disparo Secundario
    -Disparo
    -Efectos Visuales

--- Juego/Naves/Jugador/Jugador.gd
+++ Juego/Naves/Jugador/Jugador.gd
@@ -4,11 +4,30 @@ extends RigidBody2D
 export var potencia_motor:int = 20 
 export var potencia_rotacion:int = 280
 
+
+#Atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
 
 
+#Atributos Onready
+onready var canion:Canion = $Canion
+onready var laser:RayoLaser = $LaserBeam2D
+
+
+
 #Metodos
+func _unhandled_input(event: InputEvent) -> void:
+	#Disparo Rayo
+	if event.is_action_pressed("disparo_secundario"):
+		laser.set_is_casting(true)
+	
+	if event.is_action_released("disparo_secundario"):
+		laser.set_is_casting(false)
+
+
+
+
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
@@ -16,6 +35,8 @@ func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 
 func _process(delta: float) -> void:
 	Jugador_input()
+
+
 
 
 #Metodos Custom
@@ -36,7 +57,13 @@ func Jugador_input() -> void:
 	elif Input.is_action_pressed("rotar_horario"):
 		dir_rotacion += 1
 	
-
+	
+	#Disparo
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+	
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)
 
 
 
